261092c6bb68c5d43af7d906c71462e4feb8f7db
tdukdak <tdukdak9287@bths.edu>
2022-09-20 18:43:10 -0400

    I added a print statement for my favorite food

--- src/Sample.java
+++ src/Sample.java
@@ -2,6 +2,6 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Tenzin Dukdak");
         System.out.println("Everything Everywhere All at Once");
-        System.out.println("Eel avocado sushi");
+        System.out.println("My favorite food is Eel avocado sushi");
     }
 }
